814427fd89d33ce0bd9cedc5a98f3fb6fac07cfd
Amanda Azallia <amandazallia@gmail.com>
2021-08-06 11:41:17 +0700

    add project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:aplikasi_wisata/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:aplikasi_wisata/detail_screen.dart';
+import 'package:aplikasi_wisata/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
